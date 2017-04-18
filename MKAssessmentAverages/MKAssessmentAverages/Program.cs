1b7fe885ad4c27cc275e8a5bbdc14621d03bb0f7
Matthew Kersley <MWKERSLEY@users.noreply.github.com>
2017-04-18 07:54:17 +0100

    Add files via upload

---
+++ MKAssessmentAverages/MKAssessmentAverages/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKAssessmentAverages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialise variables for the do-while loops
+            char loopCheck = new char(); //option selector
+            string inputString = ""; //for reading in numbers
+            int inputNumber = 0;//to put numbers into list
+            List<int> numberList = new List<int>() { };//the list of numbers
+
+            do //loop to add numbers to an array
+            {
+                Console.WriteLine("Would you like to add a number to the list? (y/n):");
+                try //catch error if there is no input
+                {
+                    //grab input
+                    loopCheck = Convert.ToChar(Console.ReadLine().ToLower()[0]);
+                }
+                catch (IndexOutOfRangeException) //can't index 0 of empty string
+                {
+                    loopCheck = 'e'; //re-enter the loop if no input
+                }
+                if (loopCheck == 'y') //if user wants to input a number
+                {
+                    Console.WriteLine("Please enter a number, followed by return:");
+                    inputString = Console.ReadLine();
+                    if (int.TryParse(inputString, out inputNumber)) //if it is a number
+                    {
+                        numberList.Add(inputNumber); //add to list
+                    }
+                }
+                else if (loopCheck!='n')
+                {
+                    Console.WriteLine("Please enter either 'y' or 'n'.");
+                }
+            } while (loopCheck != 'n'); //continue adding numbers to list
+
+            //once all numbers entered
+            if (numberList.Count>0) //if there have been numbers entered
+            {
+                do
+                {
+                    Console.WriteLine("\nType ‘A’ to get the average of the list, 'D' to display the list, ‘S’ to sort it, 'T' to totalise numers, 'M' for the median, 'O' for the mode, or ‘X’ to exit:");
+                    try //catch error if there is no input
+                    {
+                        //grab input
+                        loopCheck = Convert.ToChar(Console.ReadLine().ToLower()[0]);
+                    }
+                    catch (IndexOutOfRangeException) //can't index 0 of empty string
+                    {
+                        loopCheck = 'e'; //re-enter the loop if no input
+                        Console.WriteLine("Not a valid input");
+                    }
+                    switch (loopCheck)//different output options
+                    {
+                        case 'a'://show average
+                            Console.WriteLine("The average is: {0}", numberList.Average());
+                            break;
+                        case 's':
+                            numberList.Sort();//sort list
+                            Console.WriteLine("List sorted as follows:");
+                            foreach (int i in numberList)
+                            {
+                                Console.Write(i + ", ");
+                            }
+                            Console.WriteLine();
+                            break;
+                        case 'd'://display list
+                            Console.WriteLine("Here is the list:");
+                            foreach (int i in numberList)
+                            {
+                                Console.Write(i + ", ");
+                            }
+                            Console.WriteLine();
+                            break;
+                        case 't'://show total
+                            Console.WriteLine("The total of all these numbers is: {0}", numberList.Sum());
+                            break;
+                        case 'm'://median
+                            List<int> tempList = numberList.ToList();
+                            tempList.Sort();
+                            if ((numberList.Count%2)==0)
+                            {
+                                Console.WriteLine("The median of these numbers is: {0}", (double)(tempList[tempList.Count / 2] + tempList[(tempList.Count / 2) - 1]) / 2);
+                            } else
+                            {
+                                Console.WriteLine("The median of these numbers is: {0}", tempList[(int)tempList.Count / 2]);
+                            }
+                            break;
+                        case 'o'://mode
+                            int[] countArray = new int[numberList.Count];//an array to match the elements of the main List to the number of their occurances
+                            int enumerator = 0; //increment index of array, to use foreach
+                            foreach (int i in numberList) //compare every element to every element
+                            {
+                                int count = 0;
+                                foreach (int j in numberList)
+                                {
+                                    if (i == j) //if the number appears, increment
+                                    {
+                                        count++;
+                                    }
+                                }
+                                countArray[enumerator] = count;
+                                enumerator++;
+                            }
+                            Console.WriteLine("The mode(s): ");
+                            List<int> duplicates = new List<int>();//to make sure modes aren't listed more than once
+                            //output mode numbers only once, checking each number in numberList against its count
+                            for(int k=0;k< countArray.Length; k++)
+                            {
+                                if ((countArray.Max() == countArray[k]) && (duplicates.IndexOf(numberList[k]))==-1)
+                                {
+                                    Console.Write(numberList[k] + ", ");
+                                    duplicates.Add(numberList[k]);
+                                }
+                            }
+                            Console.WriteLine();
+                            break;
+                        case 'x'://exit
+                            Console.WriteLine("The program will now close.");
+                            break;
+                        default://not valid character
+                            Console.WriteLine("Please enter a valid option.");
+                            break;
+                    }
+                } while (loopCheck != 'x');
+            } else //list empty
+            {
+                Console.WriteLine("List of numbers is empty. Program terminating.");
+            }
+        }
+    }
+}
+
+/* Plan: Create 2 do-while loops which ask for y, n then a, d, s, x
+ * read input at the top of the do-while
+ * if/switch on the input key
+ * ask for a number/display the appropriate list action
+ * check all inputs are valid, re-ask if not
+ */
+/* Test: I tested all requested options and all entered invalid responses at each point
+ * All outputs appeared as expected, eventually.
+ * Where there were errors they could be quickly found as the logic here is fairly simple
+ * Used breakpoints to check that 'countArray' was being populated correctly.*/
---
+++ MKAssessmentBooks/MKAssessmentBooks/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKAssessmentBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pre-build library of books
+            List<Book> library = new List<Book>() { };
+            Book book1 = new Book("To Kill A Mocking Bird", "Harper Lee");
+            Book book2 = new Book("Harry Potter", "JK Rowling", 4674960354825, 12.99m);
+            Book book3 = new Book("The Hobbit", "JRR Tolkien", 3765495736548);
+            library.Add(book1);
+            library.Add(book2);
+            library.Add(book3);
+
+            //greet
+            Console.WriteLine("Welcome to the book management system.");
+
+            //do-while check char
+            char action = new char();
+            //enter book management loop
+            do
+            {
+                //request input
+                Console.WriteLine("Press 'a' to add a book, 'd' to delete a book, 'v' to view all books, 'f' to search for a book, 't' for the total book value, 's' to sort books, 'x' to exit.");
+                //check for valid input
+                if (char.TryParse(Console.ReadLine(), out action))
+                {
+                    switch (action) //take appropriate action
+                    {
+                        case 'a':
+                            //input book details
+                            Console.Write("Please input a book title: ");
+                            string title = Console.ReadLine();
+                            Console.Write("Please input a book author: ");
+                            string author = Console.ReadLine();
+                            Console.Write("Please input the book's ISBN: ");
+                            long ISBN;
+                            long.TryParse(Console.ReadLine(), out ISBN);
+                            Console.Write("Please input a price: ");
+                            decimal price;
+                            decimal.TryParse(Console.ReadLine(), out price);
+                            //create then add book, to use showInfo method more easily
+                            Book newBook = new Book(title, author, ISBN, price);
+                            library.Add(newBook);
+                            newBook.showInfo();
+                            break;
+                        case 'd':
+                            Console.Write("Please input the book title to be deleted: ");
+                            title = Console.ReadLine().ToLower();//get book to be deleted by title
+                            bool check = false;
+                            for (int i = 0; i < library.Count; i++)
+                            {
+                                if (title==library[i].title.ToLower())//compare to all books in list
+                                {
+                                    Console.WriteLine(library[i].title + " has been removed from the system.");
+                                    library.RemoveAt(i);
+                                    Book.totalBooks--;
+                                    check = true;
+                                    break;//delete found book, exit loop
+                                }
+                            }
+                            if (!check)//if not found
+                            {
+                                Console.WriteLine(title + " is not in the system.");
+                            }
+                            break;
+                        case 'f':
+                            Console.Write("Please input the book title to be found: ");
+                            title = Console.ReadLine().ToLower();//get book to be found
+                            check = false;
+                            for (int i = 0; i < library.Count; i++)
+                            {
+                                if (title == library[i].title.ToLower())
+                                {
+                                    library[i].showInfo();
+                                    check = true;
+                                    break;//if found, show info and break loop
+                                }
+                            }
+                            if (!check) //if not found in list
+                            {
+                                Console.WriteLine(title + " is not in the system.");
+                            }
+                            break;
+                        case 's'://sort library bubble
+                            for (int k = 0; k < library.Count; k++)//passes
+                            {
+                                for (int i = 0; i < library.Count; i++)//compare each book
+                                {
+                                    for (int j = i + 1; j < library.Count; j++)//to every other book once
+                                    {
+                                        if (string.Compare(library[i].title, library[j].title) == 1) //if out of order, swap
+                                        {
+                                            Book tempBook = new Book(library[i].title, library[i].author, library[i].ISBN, library[i].price);
+                                            Book.totalBooks--;
+                                            library[i] = library[j];
+                                            library[j] = tempBook;
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            break;
+                        case 'v'://display all books by looping over library
+                            Console.WriteLine("Number of books: "+Book.totalBooks);
+                            foreach(Book book in library)
+                            {
+                                book.showInfo();
+                            }
+                            break;
+                        case 't'://display total value of 'library'
+                            Book.totalBookValue(library);
+                            break;
+                        case 'x':
+                            Console.WriteLine("Closing book manager."); //parting message
+                            break;
+                        default://unrecognised character
+                            Console.WriteLine("Unrecognised command.");
+                            break;
+                    }
+                } else //if input invalid
+                {
+                    Console.WriteLine("Unrecognised command.");
+                }
+                Console.WriteLine();
+            } while (action!='x');
+            
+        }
+    }
+    class Book //book class, for storing all individual book data
+    {
+        //attributes
+        public string title;
+        public string author;
+        public long ISBN;
+        public decimal price;
+        public bool ISBNCheck;
+
+        public static int totalBooks;
+
+        public Book(string title  = "Unknown", string author = "Unknown", long ISBN = 0, decimal price = 4.99m)
+        {
+            this.title = title;
+            this.author = author;
+            this.ISBN = ISBN;
+            this.price = price;
+            if (title == "")
+            {
+                this.title = "Unknown";
+            }
+            if (author == "")
+            {
+                this.author = "Unknown";
+            }
+            if (price == 0)
+            {
+                this.price = 4.99m;
+            }
+            if (this.ISBN!=0)
+            {
+                this.ISBNCheck = true;
+            }
+            totalBooks++;
+        }
+
+        public void showInfo()
+        {
+            if (ISBNCheck)//don't display 0 ISBN numbers
+            {
+                Console.WriteLine("{0} by {1}, ISBN - {2}, Price £{3}", this.title, this.author, this.ISBN, this.price);
+            } else
+            {
+                Console.WriteLine("{0} by {1}, ISBN - {2}, Price £{3}", this.title, this.author, "Unknown", this.price);
+            }
+        }
+
+        public static void totalBookValue(List<Book> allBooks)//sum up book prices in a list of books
+        {
+            decimal sum = 0m;
+            foreach(Book book in allBooks)//loop over all books in the list
+            {
+                sum += book.price;//add price
+            }
+            Console.WriteLine("Total price of all books: £{0}.", sum);//display total
+        }
+    }
+}
+
+/* Plan: Create Book object
+ * give it properties of title, author, ISBN and price
+ * give it a constructor to create a book
+ * give it a method to change the price
+ * create a 'library'- List of Book
+ * create a do while loop to give the option to manage the list of books in various ways
+ * give the option to sort and search for books
+ * 
+ * Testing: Checked basic loop was functioning
+ * then added first attempt for each option.
+ * Checked all options for correct functionality
+ * Checked all options to respond to bad input */
